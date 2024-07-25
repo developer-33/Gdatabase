88070897becda312cccdd1722e3f974949092269
fletcher burton <burton.fletcher@aol.com>
2024-07-25 08:00:26 -0500

    commit5

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,6 +1,21 @@
 import { useState,useEffect} from 'react';
 import Mainsection from './components/Mainsection';
 import  NavBar from './components/NavBar'
+import "../guns/ak-47.png"
+import "../guns/ASM10.png"
+import "../guns/BK57.PNG"
+import "../guns/dr-h.png"
+import "../guns/fr.556.png"
+import "../guns/hbra3.png"
+import "../guns/hvk-30.png"
+import "../guns/ICR-1.PNG"
+import "../guns/kn44.png"
+import "../guns/LK24.PNG"
+import "../guns/M16.PNG"
+import "../guns/man0war.png"
+import "../guns/M4.PNG"
+import "../guns/Type25.PNG"
+
 
 import "./index.css"
 
